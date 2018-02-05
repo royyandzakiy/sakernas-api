b873251e6257a6e690c8514a9246194882bc3924
royyandzakiy <royyan.abdul@gmail.com>
2018-02-05 10:44:57 +0700

    perbaikan db menuju collection sakernas

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://root:root@ds211088.mlab.com:11088/note_taker"
+  //url: "mongodb://root:root@ds211088.mlab.com:11088/note_taker"
+  url: "mongodb://root:root@ds123658.mlab.com:23658/sakernas"
 };
